c8267c386a7366efa1737ea304c86a9dce87fdc0
ac2pic <ac2pic@gmail.com>
2022-07-29 17:06:54 -0500

    Consolidate savebin python scripts

--- save_editor.py
+++ save_editor.py
@@ -1,7 +1,139 @@
-import pickle
+#!/usr/bin/python3
+import struct
+import sys
+filename = sys.argv[1]
+data = None
+
+try:
+    with open(filename, 'rb+') as file:
+        data = bytearray(file.read())
+except FileNotFoundError as e:
+    print("Please supply a path to a gravity rush 2 save file.")
+    exit(-1)
+
+def get_varname(arr, chunkOffset):
+    stringOffset = int.from_bytes(arr[chunkOffset: chunkOffset + 4], 'little')
+    index = stringOffset
+    while arr[index] != 0:
+        index += 1
+    return arr[stringOffset:index].decode('utf-8')
+    
+def get_type(arr, chunkOffset):
+    return arr[chunkOffset + 0x4] % 8
+
+def get_value_offset(arr, chunkOffset):
+    return int.from_bytes(arr[chunkOffset + 4: chunkOffset + 8], 'little') >> 4
+
+def read_vector(arr, vectorOffset):
+    return struct.unpack_from('<ffff', arr, offset=vectorOffset)
+
+def read_string(arr, stringOffset, length):
+    start = stringOffset
+    
+    if length == 0:
+        end = start
+    else:
+        end = stringOffset + length - 1 # remove one because null terminated
+
+    strBytes = arr[start:end]
+    try:
+        return strBytes.decode('utf-8')
+    except:
+        pass
+    return strBytes # Only so it can be converted to json
+
+def parse_dict(parent, count, metadata):
+    for _ in range(count):
+        parse_chunk(parent, metadata)
+
+def parse_chunk(root, metadata):
+    metadata["count"] += 1
+    
+    chunkOffset = metadata["offset"]
+    metadata["offset"] += 0x10
+    arr = metadata["buffer"]
+    
+    varName = get_varname(arr, chunkOffset)
+    dataType = get_type(arr, chunkOffset)
+    valueOffset = get_value_offset(arr, chunkOffset)
+    varBytesValue = arr[chunkOffset+0x8:chunkOffset+0xC]
+
+    if arr[chunkOffset + 0x4] & 8 == 0:
+        root[varName] = (dataType, None)
+        return
+    
+    if dataType == 0:
+        dictRoot = {}
+        count = int.from_bytes(varBytesValue, 'little')
+        parse_dict(dictRoot, count, metadata)
+        root[varName] = dictRoot
+        # dictionary
+    elif dataType == 1:
+        # float32
+        root[varName] = struct.unpack('<f', varBytesValue)[0]
+    elif dataType == 2:
+        # vector
+        root[varName] = read_vector(arr, valueOffset)
+    elif dataType == 3:
+        # string
+        strLength = int.from_bytes(varBytesValue, 'little')
+        root[varName] = read_string(arr, valueOffset, strLength)
+    elif dataType == 4:
+        # boolean
+        root[varName] = arr[chunkOffset+0x8] > 0
+    else:
+        # unknown
+        pass
+
+def convert_to_dict(saveData):
+    numOfData = int.from_bytes(saveData[0xC: 0x10], 'little')
+    metadata = {
+        "offset": 0x10,
+        "buffer": saveData, 
+        "count": 0
+    }
+    root = {}
+    while metadata["count"] < numOfData:
+        parse_chunk(root, metadata)
+    return root
+
+saveDict = convert_to_dict(data)
+
+# import json
+patches = []
+
+def do_patch(dictionary, patches):
+    for [path, value] in patches:
+        subDict: dict = dictionary
+        # from first to second to lest
+        pathLength = len(path)
+        pathLastIndex = max(pathLength - 1, 0)
+        for keyIndex in range(pathLastIndex):
+            key = path[keyIndex]
+            if subDict.get(key) == None:
+                subDict[key] = {}
+                continue
+            subDict = subDict[key]
+        valueKey = path[pathLastIndex]
+        if type(value) == list:
+            value = tuple(value)
+        subDict[valueKey] = value
+
+for patch in patches:
+    do_patch(saveDict, patches)
+
 import copy
 import struct
-from fnvhash import fnv1a_32
+
+def fnv1a_32(char_bytes):
+    prime = 0x01000193
+    offset_basis = 0x811c9dc5
+    h = offset_basis
+
+    for char_byte in char_bytes:
+        h = h ^ char_byte
+        h = (h * prime) % 0x100000000
+    return h
 
 def analyze_dict(dictionary, results): 
     for (key, value) in dictionary.items():
@@ -121,8 +253,8 @@ def dict_to_savebin(dictionary):
         serialize_chunk(value, key, buffer, write_pointers)
     return buffer
 
-saveDict = {}
 
 saveBuffer = dict_to_savebin(saveDict)
+
 with open('out.bin', 'wb') as out:
     out.write(saveBuffer)
